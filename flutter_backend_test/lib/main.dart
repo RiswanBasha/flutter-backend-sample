1d5748b163de47a8fda8606a68fa6039ac1da859
RiswanBasha <riswanbasha26@gmail.com>
2020-05-29 14:34:37 +0530

    second commit

--- flutter_backend_test/lib/main.dart
+++ flutter_backend_test/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Database Demo',
+      title: 'Flutter Backend Database Sample',
       theme: ThemeData.dark(),
       home: Home(),
     );
