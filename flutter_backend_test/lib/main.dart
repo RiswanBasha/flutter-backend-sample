11bb814a4bc2eae266de63e60f4b1d39f66bd2d2
RiswanBasha <riswanbasha26@gmail.com>
2020-05-29 22:22:00 +0530

    updated commit

--- flutter_backend_test/lib/main.dart
+++ flutter_backend_test/lib/main.dart
@@ -1,140 +1,57 @@
-//import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-//import 'package:firebase_database/ui/firebase_animated_list.dart';
-// import 'package:firebase_core/firebase_core.dart'; not nessecary
-
-
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+import 'dart:async';
+import 'package:flutter_backend_test/splashscreen.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      home: MyApp1(),
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Backend Database Sample',
-      theme: ThemeData.dark(),
-      home: Home(),
-    );
-  }
+    )
+  );
 }
 
-class Home extends StatefulWidget {
+class MyApp1 extends StatefulWidget {
   @override
-  HomeState createState() => HomeState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class HomeState extends State<Home> {
-  List<Item> items = List();
-  Item item;
-  DatabaseReference itemRef;
-
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
+class _MyAppState extends State<MyApp1> {
   @override
   void initState() {
     super.initState();
-    item = Item("", "");
-    final FirebaseDatabase database = FirebaseDatabase.instance; 
-    itemRef = database.reference().child('items');
-    itemRef.onChildAdded.listen(_onEntryAdded);
-    itemRef.onChildChanged.listen(_onEntryChanged);
-  }
-
-  _onEntryAdded(Event event) {
-    setState(() {
-      items.add(Item.fromSnapshot(event.snapshot));
-    });
-  }
-
-  _onEntryChanged(Event event) {
-    var old = items.singleWhere((entry) {
-      return entry.key == event.snapshot.key;
-    });
-    setState(() {
-      items[items.indexOf(old)] = Item.fromSnapshot(event.snapshot);
-    });
-  }
-
-  void handleSubmit() {
-    final FormState form = formKey.currentState;
-
-    if (form.validate()) {
-      form.save();
-      form.reset();
-      itemRef.push().set(item.toJson());
+    Future.delayed(Duration(
+      seconds:3
+    ),(){
+      Navigator.pushReplacement(context,MaterialPageRoute(builder: (context)=>MyApp(),),);
     }
+    );
   }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Firebase Sample'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+          FlutterLogo(
+        size: 400,
       ),
-      resizeToAvoidBottomPadding: false,
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            flex: 0,
-            child: Center(
-              child: Form(
-                key: formKey,
-                child: Flex(
-                  direction: Axis.vertical,
-                  children: <Widget>[
-                    ListTile(
-                      leading: Icon(Icons.info),
-                      title: TextFormField(
-                        initialValue: "",
-                        onSaved: (val) => item.title = val,
-                        validator: (val) => val == item.title ? val : null,
-                      ),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.info),
-                      title: TextFormField(
-                        initialValue: "",
-                        onSaved: (val) => item.body = val,
-                        validator: (val) => val == item.body ? val : null,
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.send),
-                      onPressed: () {
-                        handleSubmit();
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+      new Text(
+        'Powered by SRB',
+         softWrap: true,
+        textAlign: TextAlign.center,
+        style: new TextStyle(
+          fontSize: 18,
+          color: Colors.white,
+          decoration: TextDecoration.none
+        ),
+        ),
+      ],
+      
+
     );
   }
 }
 
 
-class Item {
-  String key;
-  String title;
-  String body;
 
-  Item(this.title, this.body);
 
-  Item.fromSnapshot(DataSnapshot snapshot)
-      : key = snapshot.key,
-        title = snapshot.value["title"],
-        body = snapshot.value["body"];
-
-  toJson() {
-    return {
-      "title": title,
-      "body": body,
-    };
-  }
-}
